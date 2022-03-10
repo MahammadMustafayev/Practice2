4c7946dca2861504143a359a8f679586bce88067
Mahammad Mustafayev <tu7ekdm78@aztu.local>
2022-03-10 14:35:17 +0400

    Added

---
+++ UniTask1/UniTask1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UniTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, -2, 1, 4, -6, 8 };
+            NegativetoPositive(ref numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+        }
+
+        static int NegativetoPositive(ref int [] numbers)
+        {
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]<0)
+                {
+                    numbers[i]=numbers[i] *(-1);
+                }          
+            }
+            return 0;
+        }
+    }
+}
